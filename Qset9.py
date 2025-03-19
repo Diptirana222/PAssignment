f301fe1898f23ca4bf6dc1dfee81e3da666e95fc
Dipti Rana <diptiranaji22@gmail.com>
2025-03-19 20:17:31 +0530

    Add files via upload

---
+++ Qset9.py
@@ -0,0 +1,53 @@
+#Question based on operator presedence 
+#Q1
+a = 25 // 4 + 3 * 2
+print(a)
+
+#q2
+o =  5 + 3 ** 2 // 4 * 2
+print(o)
+
+#q3
+g = (12 - 4) * 3 // 2 + 5 
+print(g)
+
+#q4
+b = (2 ** 3 * 4 // 2 + 6 - 3)
+print(b)
+
+#q5
+x =  (50 % 7 + 3 * 2 ** 3)
+print(x)
+
+#q6
+y =((20 - 5) * 3 // 2 ** 2 + 4)
+print(y)  
+
+#q7
+a = (10 - 2) * 3 ** 2
+b = 10 - 2 * 3 ** 2
+print(a)
+print(b)
+print(a==b)
+
+#q8
+x = 3 ** 2 * 4 // 2 + 1 
+y = (3 ** 2) * (4 // 2) + 1 
+print(x==y) 
+     
+#q9
+q =  4 + 5 * 2 ** 3 - 6 // 3 
+print(q)
+
+q = ((4 + 5) * 2) ** (3 - 6 // 3) 
+print(q)
+
+q = 4 + (5 * 2) ** (3 - 6 // 3)  
+print(q)
+#third option is right
+
+#q10
+p = 30 / 5 + 2 ** 3 * 2 - 1
+print(p)
+l = ((30 / 5) + 2) ** (3 * 2 - 1)
+print(l)
